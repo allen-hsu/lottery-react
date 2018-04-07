ab2c9764f02f69750b3b54f3d2a122fdfc447ad0
Allen Hsu <allenhsu.taiwan@gmail.com>
2018-04-08 00:40:39 +0800

    fix if not install metamask error

--- src/web3.js
+++ src/web3.js
@@ -5,7 +5,7 @@ var web3;
 if(typeof window.web3 !== 'undefined') {
     web3 = new Web3(window.web3.currentProvider);
 } else {
-    web3 = new Web3();
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
 export default web3;
